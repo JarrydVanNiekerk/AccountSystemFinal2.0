563b229cdb8ac7301b78c2e204c3c5e5d27d1300
Jarryd van Niekerk <vanniekerkjarryd@gmail.com>
2021-10-11 17:21:47 +0200

    Exception handeling

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/ModifyAccountTypeFlowImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/ModifyAccountTypeFlowImpl.java
@@ -11,20 +11,32 @@ import javax.transaction.Transactional;
 @Component
 public class ModifyAccountTypeFlowImpl implements ModifyAccountTypeFlow {
 
-
+    public String msg = "Action could not be performed";
     private final AccountTypeTranslator translator;
 
     public ModifyAccountTypeFlowImpl(AccountTypeTranslator  translator) {
-        this.translator = translator;
+        try {
+            this.translator = translator;
+        }catch(Exception e){
+            throw new RuntimeException(msg);
+        }
     }
 
     @Override
     public AccountTypeDto deleteAccountTypeByMnemonic(String mnemonic) {
+        try{
         return translator.deleteAccountTypeByMnemonic(mnemonic);
+        }catch(Exception e){
+            throw new RuntimeException(msg);
+        }
     }
 
     @Override
     public AccountTypeDto updateAccountType(AccountTypeDto accountType) {
+        try{
         return translator.updateAccountType(accountType);
+        }catch(Exception e){
+            throw new RuntimeException(msg);
+        }
     }
 }
